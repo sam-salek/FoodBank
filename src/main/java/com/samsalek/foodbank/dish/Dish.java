18f57c63a876466001881102aa80d807596c5694
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2024-03-16 14:12:39 +0100

    Added default values to dish.

--- src/main/java/com/samsalek/foodbank/dish/Dish.java
+++ src/main/java/com/samsalek/foodbank/dish/Dish.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -16,18 +17,19 @@ public class Dish {
     private String id;
 
     private String name;
-    private String category;
-    private List<String> ingredients;
-    private LocalDateTime created;
+    private String category = "Unknown";
+    private List<String> ingredients = new ArrayList<>();
+    private List<String> instructions = new ArrayList<>();
+    private LocalDateTime creationTime = LocalDateTime.now();
 
     public Dish(String name) {
         this.name = name;
     }
 
-    public Dish(String name, String category, List<String> ingredients, LocalDateTime created) {
+    public Dish(String name, String category, List<String> ingredients, List<String> instructions) {
         this.name = name;
         this.category = category;
         this.ingredients = ingredients;
-        this.created = created;
+        this.instructions = instructions;
     }
 }
