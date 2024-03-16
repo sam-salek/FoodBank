a49c34b9768182efbee08571fc9c604c3526ded5
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2024-03-16 14:11:41 +0100

    Updated dish service to use id more frequently.

--- src/main/java/com/samsalek/foodbank/dish/DishService.java
+++ src/main/java/com/samsalek/foodbank/dish/DishService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -19,34 +20,43 @@ public class DishService {
 
     public Dish getDishByName(String name) {
         List<Dish> dishes = dishRepository.findByName(name);
-        if (dishes.isEmpty()) throw new IllegalStateException("No dishes with name: " + name);
+        if (dishes.isEmpty()) throw new IllegalArgumentException("No dishes with name: " + name);
         return dishes.get(0);
     }
 
+    public Dish getDishById(String id) {
+        Optional<Dish> dish = dishRepository.findById(id);
+        if (dish.isEmpty()) throw new IllegalArgumentException("No dishes with id: " + id);
+        return dish.get();
+    }
+
     public Dish addDish(Dish dish) {
-        boolean exists = dishRepository.existsByName(dish.getName());
-        if (exists) throw new IllegalStateException("Dish with name \"" + dish.getName() + "\" already exists!");
+        // Name check
+        if (dish.getName() == null) throw new IllegalArgumentException("Dish name cannot be null!");
+        if (dish.getName().length() == 0) throw new IllegalArgumentException("Dish name cannot be of length 0!");
+        if (dishRepository.existsByName(dish.getName())) throw new IllegalArgumentException("Dish with name \"" + dish.getName() + "\" already exists!");
+
         return dishRepository.save(dish);
     }
 
-    public Long deleteDishByName(String name) {
-        boolean exists = dishRepository.existsByName(name);
-        if (!exists) throw new IllegalStateException("No dishes with name: " + name);
-        return dishRepository.deleteByName(name);
+    public void deleteDishById(String id) {
+        // Id check
+        if (dishRepository.findById(id).isEmpty()) throw new IllegalArgumentException("No dishes with id: " + id);
+
+        dishRepository.deleteById(id);
     }
 
-    public Dish updateDish(String name, String newName, String newCategory, List<String> newIngredients) {
-        List<Dish> dishes = dishRepository.findByName(name);
-        if (dishes.isEmpty()) throw new IllegalStateException("No dishes with name: " + name);
-        Dish dish = dishes.get(0);
+    public Dish updateDish(String id, Dish dish) {
+        // Id check
+        if (dishRepository.findById(id).isEmpty()) throw new IllegalArgumentException("No dishes with id: " + id);
 
-        if (newName != null && newName.length() > 0 && !newName.equals(name)) {
-            if (dishRepository.existsByName(newName)) throw new IllegalStateException("Dish with name \"" + newName + "\" already exists!");
-            dish.setName(newName);
-        }
-        if (newCategory != null && newCategory.length() > 0 && !newCategory.equals(dish.getCategory())) dish.setCategory(newCategory);
-        if (newIngredients != null && newIngredients.size() > 0) dish.setIngredients(newIngredients);
+        // Name check
+        if (dish.getName() == null) throw new IllegalArgumentException("Dish name cannot be null!");
+        if (dish.getName().length() == 0) throw new IllegalArgumentException("Dish name cannot be of length 0!");
+        if (dishRepository.existsByName(dish.getName())) throw new IllegalArgumentException("Dish with name \"" + dish.getName() + "\" already exists!");
 
-        return dishRepository.save(dish);
+       dishRepository.deleteById(id);
+       dish.setId(id);
+       return dishRepository.save(dish);
     }
 }
