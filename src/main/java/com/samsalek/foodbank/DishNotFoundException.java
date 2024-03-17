8eac1312b35fd3f5fb99847cf2940c30c4c4f14e
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2024-03-18 00:52:55 +0100

    Added new exceptions

---
+++ src/main/java/com/samsalek/foodbank/DishNotFoundException.java
@@ -0,0 +1,20 @@
+package com.samsalek.foodbank;
+
+import com.samsalek.foodbank.dish.Dish;
+
+public class DishNotFoundException extends RuntimeException {
+
+    private static final String BASE_MESSAGE = "Unable to find dish";
+
+    public DishNotFoundException() {
+        super(BASE_MESSAGE);
+    }
+
+    public DishNotFoundException(Dish dish) {
+        super(String.format("%s with id %s and name %s", BASE_MESSAGE, dish.getId(), dish.getName()));
+    }
+
+    public DishNotFoundException(String dishId) {
+        super(String.format("%s with id %s", BASE_MESSAGE, dishId));
+    }
+}
---
+++ src/main/java/com/samsalek/foodbank/InvalidDishNameException.java
@@ -0,0 +1,24 @@
+package com.samsalek.foodbank;
+
+public class InvalidDishNameException extends IllegalArgumentException {
+
+    public InvalidDishNameException() {
+        super("Dish name is invalid");
+    }
+
+    public InvalidDishNameException(String message) {
+        super(message);
+    }
+
+    public static InvalidDishNameException nullDishName() {
+        return new InvalidDishNameException("Dish name cannot be null");
+    }
+
+    public static InvalidDishNameException emptyDishName() {
+        return new InvalidDishNameException("Dish name cannot be of length 0");
+    }
+
+    public static InvalidDishNameException duplicateDishName() {
+        return new InvalidDishNameException("Dish with this name already exists");
+    }
+}
